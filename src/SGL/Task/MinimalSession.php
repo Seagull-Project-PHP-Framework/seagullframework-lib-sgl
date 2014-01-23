4aab0b353f78a1d3bbae675681720cfd9636ba14
Demian Turner <demian@phpkitchen.com>
2014-01-23 18:41:50 +0000

    no message

---
+++ src/SGL/Task/MinimalSession.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: demian
+ * Date: 23/01/2014
+ * Time: 18:33
+ */
+
+class SGL_Task_MinimalSession extends SGL_DecorateProcess
+{
+    function process($input, $output)
+    {
+        session_start();
+        $_SESSION['uid'] = 1;
+        $_SESSION['rid'] = 1;
+        $_SESSION['aPrefs'] = array();
+
+        $this->processRequest->process($input, $output);
+    }
+}
